f1886829feefc3f563aa3142bb5b625af25b4507
DevJS <devjs@MacBook-Pro-Denis.local>
2019-02-25 08:05:32 +0200

    update CRA

--- client/src/app/configure-store.js
+++ client/src/app/configure-store.js
@@ -1,14 +1,50 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import reducers from './root-reducers';
-import callApi from './../utils/apiCaller';
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+// import reducers from './root-reducers';
+// import callApi from './../utils/apiCaller';
 
-const configureStore = () => {
-  return createStore(
-    reducers,
-    composeWithDevTools(applyMiddleware(thunk.withExtraArgument(callApi)))
-  );
-};
+// const configureStore = () => {
+//   return createStore(
+//     reducers,
+//     composeWithDevTools(applyMiddleware(thunk.withExtraArgument(callApi)))
+//   );
+// };
 
-export default configureStore;
+// export default configureStore;
+
+import { createStore, applyMiddleware, compose } from 'redux'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+import thunk from 'redux-thunk'
+import createHistory from 'history/createBrowserHistory'
+import rootReducer from './modules'
+import callApi from './../utils/apiCaller'
+
+export const history = createHistory()
+const initialState = {}
+const enhancers = []
+const middleware = [
+  thunk,
+  routerMiddleware(history)
+]
+
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension())
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+)
+
+const store = createStore(
+  connectRouter(history)(rootReducer),
+  initialState,
+  composedEnhancers
+)
+
+export default store
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './app/App';
-import configureStore from './app/configure-store';
-import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import 'bootstrap/dist/css/bootstrap.css';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const store = configureStore();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'));
 
-registerServiceWorker();
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import App from './app/App';
+// import configureStore from './app/configure-store';
+// import registerServiceWorker from './registerServiceWorker';
+
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// const store = configureStore();
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>,
+//     document.getElementById('root'));
+
+// registerServiceWorker();
